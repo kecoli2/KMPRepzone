6cac38d04e72d397e2fd36f88c6130f69e29dcda
Salih Yücel <salih_yucel@outlook.com>
2025-08-29 08:52:26 +0300

    ...

--- database/src/androidMain/kotlin/com/repzone/database/driver/DatabaseDriverFactory.android.kt
+++ database/src/androidMain/kotlin/com/repzone/database/driver/DatabaseDriverFactory.android.kt
@@ -5,6 +5,7 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import com.repzone.database.AppDatabase
 
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class DatabaseDriverFactory(private val context: Context) {
     actual fun create(): SqlDriver =
         AndroidSqliteDriver(schema = AppDatabase.Schema, context = context, name = "app.db")
--- database/src/commonMain/kotlin/com/repzone/database/driver/DatabaseDriverFactory.kt
+++ database/src/commonMain/kotlin/com/repzone/database/driver/DatabaseDriverFactory.kt
@@ -2,6 +2,7 @@ package com.repzone.database.driver
 
 import app.cash.sqldelight.db.SqlDriver
 
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 expect class DatabaseDriverFactory {
     fun create(): SqlDriver
 }
