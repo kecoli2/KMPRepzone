79cc8a55711977ce93160ce58d4c05adb402569a
Salih Yücel <salih_yucel@outlook.com>
2025-10-24 17:34:27 +0300

    ....

---
+++ presentation-legacy/src/commonMain/kotlin/com/repzone/presentation/legacy/model/RepresentSummary.kt
@@ -0,0 +1,10 @@
+package com.repzone.presentation.legacy.model
+
+data class RepresentSummary(
+    val visitTotal : Int,
+    val visitDoneTotal: Int,
+    val orderCount: Int,
+    val orderValue: Double,
+    val formCount: Int,
+    val activeAppoinmentDayCount: Int
+)
